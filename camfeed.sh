79ce6942d92b83e1fe904dac586196b684f23e97
xheen908 <xheen908@gmail.com>
2019-05-31 17:43:42 +0200

    Create camfeed.sh

---
+++ camfeed.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm /etc/opkg/secret-feed.conf
+rm /etc/opkg/deb-feed.conf
+opkg remove softcam-feed-universal
+cd /tmp
+wget camfeed.digital-eliteboard.com/opkg/mips/softcam-feed-universal_4.3-r0_all.ipk
+opkg install softcam-feed-universal_4.3-r0_all.ipk
+touch /etc/opkg/deb-feed.conf
+echo 'src/gz deb-feed http://camfeed.digital-eliteboard.com/opkg/mips' >> /etc/opkg/deb-feed.conf
+opkg update
+cd
+rm camfeed.sh
+
