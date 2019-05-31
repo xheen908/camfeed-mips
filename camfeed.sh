695c3adf4a7abaf30b6f410279f65465759db170
xheen908 <xheen908@gmail.com>
2019-05-31 17:48:01 +0200

    Update camfeed.sh

--- camfeed.sh
+++ camfeed.sh
@@ -4,7 +4,7 @@ rm /etc/opkg/secret-feed.conf
 rm /etc/opkg/deb-feed.conf
 opkg remove softcam-feed-universal
 cd /tmp
-wget camfeed.digital-eliteboard.com/opkg/mips/softcam-feed-universal_4.3-r0_all.ipk
+wget --no-check-certificate https://github.com/xheen908/camfeed-mips/raw/master/softcam-feed-universal_4.3-r0_all.ipk
 opkg install softcam-feed-universal_4.3-r0_all.ipk
 touch /etc/opkg/deb-feed.conf
 echo 'src/gz deb-feed http://camfeed.digital-eliteboard.com/opkg/mips' >> /etc/opkg/deb-feed.conf
